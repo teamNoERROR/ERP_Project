57696ca6dc1da5659bf82cace694ff7cdaa50325
hjver <khj67515@naver.com>
2025-05-28 15:49:55 +0900

    MRP 계산결과 저장 및 엑셀 다운로드 기능 구현

--- kr.co.noerror/src/main/java/kr/co/noerror/DAO/mrp_DAO.java
+++ kr.co.noerror/src/main/java/kr/co/noerror/DAO/mrp_DAO.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import kr.co.noerror.DTO.mrp_result_DTO;
+import kr.co.noerror.DTO.order_DTO;
 import kr.co.noerror.DTO.plan_DTO;
 import kr.co.noerror.DTO.temp_bom_DTO;
 
@@ -26,6 +28,21 @@ public class mrp_DAO {
 	public List<temp_bom_DTO> boms_for_mrp(String bom_code){
 		List<temp_bom_DTO> boms = this.sql.selectList("boms_for_mrp", bom_code);
 		return boms;
+	}
+	
+	public 	int mrp_code_check(String mrp_code) {
+		int count = this.sql.selectOne("mrp_code_check", mrp_code);
+		return count;
+	}
+	
+	public int insert_mrp_header(String mrp_code) {
+		int result = this.sql.insert("insert_mrp_header", mrp_code);
+		return result;
+	}
+	
+	public int insert_mrp_detail(mrp_result_DTO mdto) {
+		int result = this.sql.insert("insert_mrp_detail", mdto);
+		return result;
 	}
 
 }
--- kr.co.noerror/src/main/java/kr/co/noerror/DTO/mrp_result_DTO.java
+++ kr.co.noerror/src/main/java/kr/co/noerror/DTO/mrp_result_DTO.java
@@ -1,11 +1,18 @@
 package kr.co.noerror.DTO;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@AllArgsConstructor //mrp_Calulation class에서 new mrp_result_DTO(item_code, bom.getItem_name(), bom.getItem_type(), required_qty, bom.getItem_unit(), total_stock, safety_stock, reserved_stock, available_stock, shortage_stock) 사용시 필요
+@NoArgsConstructor  //ajax JSON.stringify(data)를 @RequestBody List<mrp_input_DTO> mrp_inputs으로 받을 경우 기본생성자 필요
 public class mrp_result_DTO {
+	private String mrp_code;
     private String item_code;
     private String item_type;
     private String item_name;

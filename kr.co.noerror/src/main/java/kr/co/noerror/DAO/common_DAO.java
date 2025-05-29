0db869a0622cd543f439548a3a087da3e9ce0912
h0xxln <alxl47@naver.com>
2025-05-29 12:03:43 +0900

    [add] 2025-05-29 인바운드 창고 html수정 및 warehouse modal 생성, common 관리자 관련 함수 주석 해제, 입출고 부자제 완제품(IOSF 클래스로 관리)

--- kr.co.noerror/src/main/java/kr/co/noerror/DAO/common_DAO.java
+++ kr.co.noerror/src/main/java/kr/co/noerror/DAO/common_DAO.java
@@ -1,16 +1,37 @@
 package kr.co.noerror.DAO;
 
-//@Service
-//public class common_DAO implements common_service {
-//	Logger log = LoggerFactory.getLogger(this.getClass());
-//	
-//	@Autowired 
-//	private common_mapper cm_mapper;
-//	
-//	@Override
-//	public List<employee_DTO> emp_list() {
-//		List<employee_DTO> all_data = this.cm_mapper.emp_list();
-//		return all_data;
-//	}
-//
-//}
+import java.util.List;
+
+import org.slf4j.Logger;       
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.noerror.Mapper.common_mapper;
+import kr.co.noerror.Service.common_service;
+import kr.co.noerror.DTO.WareHouse_DTO;
+import kr.co.noerror.DTO.employee_DTO;
+
+@Service
+public class common_DAO implements common_service {
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired 
+	private common_mapper cm_mapper;
+	
+	@Override
+	public List<employee_DTO> emp_list() {
+		List<employee_DTO> all_data = this.cm_mapper.emp_list();
+		return all_data;
+	}
+	
+	@Override
+	public List<WareHouse_DTO> warehouse_list() {
+		List<WareHouse_DTO> all_data = this.cm_mapper.warehouse_list();
+		
+		return all_data;
+	}
+	
+	
+
+}
--- kr.co.noerror/src/main/java/kr/co/noerror/Mapper/common_mapper.java
+++ kr.co.noerror/src/main/java/kr/co/noerror/Mapper/common_mapper.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import kr.co.noerror.DTO.WareHouse_DTO;
 import kr.co.noerror.DTO.employee_DTO;
 
-//@Mapper
-//public interface common_mapper {
-//	
-//	List<employee_DTO> emp_list();  //관리자 리스트 전체데이터 출력
-//
-//}
+@Mapper
+public interface common_mapper {
+	
+	List<employee_DTO> emp_list();  //관리자 리스트 전체데이터 출력
+	List<WareHouse_DTO> warehouse_list(); //창고 리스트 전체 데이터 출력
+
+}
